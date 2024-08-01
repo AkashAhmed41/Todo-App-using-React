1fe5df8ef63ed3b72c62b70046a04db8151a16af
AkashAhmed41 <akash.shellbeehaken@gmail.com>
2024-08-01 15:01:30 +0600

    removed the persistent behavior for convenience

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,11 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
-import { persistReducer } from "redux-persist";
+import { configureStore } from "@reduxjs/toolkit";
 
 import todosReducer from "./todoSlice";
 
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
-
-const reducer = combineReducers({
-  todosReducer: todosReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, reducer);
-
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    todosReducer: todosReducer,
+  },
 });
 
 export default store;
