001e8c0ef1adb18f1c1fbf1a54561306fd594b32
AkashAhmed41 <akash.shellbeehaken@gmail.com>
2024-08-02 16:22:08 +0600

    added back the persistant functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 import store from "./store/store";
 
+const persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 
 import todosReducer from "./todoSlice";
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  todosReducer: todosReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = configureStore({
-  reducer: {
-    todosReducer: todosReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
